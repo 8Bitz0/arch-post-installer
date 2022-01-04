f26f107414d550d1a769fe8d652d1eeb41d42bd8
Alexander Hansen <82516534+8Bitz0@users.noreply.github.com>
2022-01-03 21:12:26 -0500

    Add basic user input functions.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #!/usr/env python3
+
+def ask_yn(text, recommended):
+    if recommended == True:
+        possible_answers = " [Y/n]"
+
+    elif recommended == False:
+        possible_answers = " [y/N]"
+
+    while True:
+        answer = input(text + possible_answers)
+        if answer == "y" or "Y":
+            return True
+
+        elif answer == "n" or "N":
+            return False
+
+        elif answer == "":
+            if recommended == True:
+                return True
+
+            elif recommended == False:
+                return False
+
+        else:
+            print("Invalid input!")
