24918d81aae9ed15d5877027f082d87d3abe15a1
8Bitz0 <alexander.hansenwalko@gmail.com>
2022-01-04 08:26:04 -0500

    Add more applications.

--- main.py
+++ main.py
@@ -36,20 +36,33 @@ def yay(package):
 
 user = input("What username should be used? ")
 
-print("-- Core")
+print("\n-- Core")
 if ask_yn("Install git?", True):
     pacman("git")
 if ask_yn("Install yay (AUR helper)?", True):
     os.system("cd /opt; sudo git clone https://aur.archlinux.org/yay-git.git; sudo chown -R " + user + ":" + user + " ./yay-git; cd yay-git; makepkg -si")
 if ask_yn("Swap pacman.conf (enable multilib included)?", True):
     os.system("sudo echo ./pacman.conf > /etc/pacman.conf")
+    os.system("sudo pacman -Syu")
 
-print("-- Utilities")
+print("\n-- Utilities")
 if ask_yn("Install Chromium?", True):
     pacman("chromium")
+if ask_yn("Install Bitwarden?", True):
+    pacman("bitwarden")
 
-print("-- Chat")
+print("\n-- Chat")
 if ask_yn("Install Discord with dvm (Discord Version Manager)? ", True):
     os.system("wget https://github.com/diced/dvm/releases/download/1.1.7/dvm-x86_64-unknown-linux-gnu")
     os.system("sudo mv dvm-x86_64-unknown-linux-gnu /bin/dvm")
-    os.system("dvm install stable")
+    os.system("/bin/dvm install stable")
+
+print("\n-- Games")
+if ask_yn("Install Steam (requires multilib)?", True):
+    pacman("steam")
+if ask_yn("Install Lutris?", True):
+    pacman("lutris")
+if ask_yn("Install GameMode?", True):
+    pacman("gamemode")
+if ask_yn("Install Heroic Games Launcher?", True):
+    yay("heroic-games-launcher-bin")
