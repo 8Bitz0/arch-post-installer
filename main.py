748b646fcd33ed46d6c3221b46ca34d7062fbdd4
8Bitz0 <alexander.hansenwalko@gmail.com>
2022-01-03 22:03:55 -0500

    Add basic applications.

--- main.py
+++ main.py
@@ -32,3 +32,24 @@ def pacman(package):
 
 def yay(package):
     os.system("yay -S " + package)
+
+
+user = input("What username should be used? ")
+
+print("-- Core")
+if ask_yn("Install git?", True):
+    pacman("git")
+if ask_yn("Install yay (AUR helper)?", True):
+    os.system("cd /opt; sudo git clone https://aur.archlinux.org/yay-git.git; sudo chown -R " + user + ":" + user + " ./yay-git; cd yay-git; makepkg -si")
+if ask_yn("Swap pacman.conf (enable multilib included)?", True):
+    os.system("sudo echo ./pacman.conf > /etc/pacman.conf")
+
+print("-- Utilities")
+if ask_yn("Install Chromium?", True):
+    pacman("chromium")
+
+print("-- Chat")
+if ask_yn("Install Discord with dvm (Discord Version Manager)? ", True):
+    os.system("wget https://github.com/diced/dvm/releases/download/1.1.7/dvm-x86_64-unknown-linux-gnu")
+    os.system("sudo mv dvm-x86_64-unknown-linux-gnu /bin/dvm")
+    os.system("dvm install stable")
